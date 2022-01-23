d3043c1bdb4dcd66bb4ab661535b6aedbdfbe532
arne-henningsen <arne.henningsen@gmail.com>
2022-01-23 12:43:03 +0100

    added further tests for logLik.sfacross()

--- tests/sfacross.R
+++ tests/sfacross.R
@@ -8,6 +8,8 @@ data( "electricity" )
 cd_u_h <- sfacross( formula = log(tc/wf) ~ log(y) + log(wl/wf) + log(wk/wf),
   udist = 'hnormal', uhet = ~ regu, data = utility, S = -1, method = 'bfgs' )
 logLik( cd_u_h )
+all.equal( c( logLik( cd_u_h ) ), 
+  sum( logLik( cd_u_h, individual = TRUE )[[ "logLik" ]] ) )
 round( coef( summary( cd_u_h ) ), 3 )
 t( sapply( efficiencies( cd_u_h ), function(x) round( summary( x ), 3 ) ) )
 
@@ -15,6 +17,8 @@ t( sapply( efficiencies( cd_u_h ), function(x) round( summary( x ), 3 ) ) )
 cd_u_t <- sfacross( formula = log(tc/wf) ~ log(y) + log(wl/wf) + log(wk/wf),
   udist = 'tnormal', muhet = ~ regu, data = utility, S = -1, method = 'bhhh' )
 logLik( cd_u_t )
+all.equal( c( logLik( cd_u_t ) ), 
+  sum( logLik( cd_u_t, individual = TRUE )[[ "logLik" ]] ) )
 round( coef( summary( cd_u_t ) ), 3 )
 t( sapply( efficiencies( cd_u_t ), function(x) round( summary( x ), 3 ) ) )
 
@@ -23,6 +27,8 @@ cd_u_ts <- sfacross( formula = log(tc/wf) ~ log(y) + log(wl/wf) + log(wk/wf),
   udist = 'tnormal', muhet = ~ regu, uhet = ~ regu, data = utility, S = -1,
   scaling = TRUE, method = 'mla' )
 logLik( cd_u_ts )
+all.equal( c( logLik( cd_u_ts ) ), 
+  sum( logLik( cd_u_ts, individual = TRUE )[[ "logLik" ]] ) )
 round( coef( summary( cd_u_ts ) ), 3 )
 t( sapply( efficiencies( cd_u_ts ), function(x) round( summary( x ), 3 ) ) )
 
@@ -31,6 +37,8 @@ t( sapply( efficiencies( cd_u_ts ), function(x) round( summary( x ), 3 ) ) )
 cd_p_ge <- sfacross( formula = log(PROD) ~ log(AREA) + log(LABOR) + log(NPK) +
     log(OTHER), udist = 'genexponential', data = ricephil, S = 1, method = 'bfgs')
 logLik( cd_p_ge )
+all.equal( c( logLik( cd_p_ge ) ), 
+  sum( logLik( cd_p_ge, individual = TRUE )[[ "logLik" ]] ) )
 round( coef( summary( cd_p_ge ) ), 3 )
 t( sapply( efficiencies( cd_p_ge ), function(x) round( summary( x ), 3 ) ) )
 
@@ -41,5 +49,7 @@ cd_u_g <- sfacross( formula = log(cost/fprice) ~ log(output) +
   data = electricity, S = -1, method = 'bfgs', simType = 'halton', Nsim = 200,
   hessianType = 2 )
 logLik( cd_u_g )
+all.equal( c( logLik( cd_u_g ) ), 
+  sum( logLik( cd_u_g, individual = TRUE )[[ "logLik" ]] ) )
 round( coef( summary( cd_u_g ) ), 3 )
 t( sapply( efficiencies( cd_u_g ), function(x) round( summary( x ), 3 ) ) )
