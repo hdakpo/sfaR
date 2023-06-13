a76eb5d83d73422882b441a47d1f690669bb5267
ydesjeux <yann.desjeux@inra.fr>
2023-06-13 11:27:50 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
     packageStartupMessage(paste0("* Please cite the 'sfaR' package as:\n", 
         "  Dakpo KH., Desjeux Y., Henningsen A., and Latruffe L. (2023). sfaR: Stochastic Frontier Analysis Routines. R package version 1.0.0.\n\n", 
         "See also: citation(\"sfaR\")\n\n", 
-        "* For any questions, suggestions, or comments on the 'sfaR' package, please make use of Tracker facilities at:\n",
+        "* For any questions, suggestions, or comments on the 'sfaR' package, you can contact directly the authors or visit:\n",
         "  https://github.com/hdakpo/sfaR/issues"))
 }
