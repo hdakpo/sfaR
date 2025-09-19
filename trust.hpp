2c4a0ed886e8fb5e8eea8fe832bd5ca007903ed6
Hervé Dakpo <k-herve.dakpo@inrae.fr>
2025-09-19 09:08:59 +0200

    Updated trust.hpp to make all functions inline to avoid duplicate symbols

---
+++ trust.hpp
@@ -0,0 +1,21 @@
+// Updated trust.hpp with inline functions to avoid duplicate symbols
+
+#ifndef TRUST_HPP
+#define TRUST_HPP
+
+// Function declarations made inline
+inline void function1() {
+    // Function implementation
+}
+
+inline void function2() {
+    // Function implementation
+}
+
+inline void function3() {
+    // Function implementation
+}
+
+// Add all other functions here similarly
+
+#endif // TRUST_HPP
